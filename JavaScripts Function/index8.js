5cdbd32f9b5ade0202fb3c24ce3812c35e9289a6
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-01-17 15:32:43 +0500

    Update index8.js

--- JavaScripts Function/index8.js
+++ JavaScripts Function/index8.js
@@ -1,14 +1,14 @@
-function fn() {
-        let employees = [
-          'Bilal',
-          'Zahra',
-          'Ali',
-          'Sufyan',
-          'Waqas',
-          'Salman',
-          'Ayesha',
-        ];
-        let random = employees[(Math.random() * employees.length) | 0];
-        document.write(random);
-      }
-      fn();
+let employees = [
+"Bilal",
+"Zahra",
+"Ali",
+"Sufyan",
+"Waqas",
+"Salman",
+"Ayesha",
+];
+
+function fn(workers) {
+return employees[(Math.random() * employees.length) | 0];
+}
+document.write(fn());
