9726469236b6ffac2b519835f582ce5e8f6300b6
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-01-17 15:29:22 +0500

    Update index7.js

--- JavaScripts Function/index7.js
+++ JavaScripts Function/index7.js
@@ -1,6 +1,5 @@
-function fn() {
-        let arr = [3, 2, 5, 7];
-        let check = arr.includes(5);
-        document.write(check);
-      }
-      fn();
+let arr = [3, 2, 5, 7];
+function fn(exists) {
+return arr.includes(5);
+}
+document.write(fn());
