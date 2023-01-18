2a4a6783a9b6ca01b641e9be8afcd548c98d83a5
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-01-18 16:23:42 +0500

    Update index5.js

--- JavaScripts Function/index5.js
+++ JavaScripts Function/index5.js
@@ -1,5 +1,14 @@
+"IN-BUILD FUNCTION"
 let subjects = ['Physics', 'Computer', 'Islamiat', 'English', 'Math'];
 subjects.sort(function (mix) {
 return 0.5 - Math.random();
 });
+document.write(subjects);
+
+"OUTSIDE"
+let subjects = ['Physics', 'Computer', 'Islamiat', 'English', 'Math'];
+function rand(mix) {
+return 0.5 - Math.random();
+}
+subjects.sort(rand);
 document.write(subjects);
